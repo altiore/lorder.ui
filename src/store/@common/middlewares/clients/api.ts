766f5cd6781b4a999ef45bebfcc7f84d98d4e39d
Razzwan <razvanlomov@gmail.com>
2018-07-04 14:27:15 +0300

    fix(chore): fix base API url

--- src/store/@common/middlewares/clients/api.ts
+++ src/store/@common/middlewares/clients/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://altiore.herokuapp.com/v1';
+const baseURL = 'https://staging-altiore-api.herokuapp.com/v1';
 // const baseURL = 'http://localhost:3000/v1';
 
 export const api = axios.create({
