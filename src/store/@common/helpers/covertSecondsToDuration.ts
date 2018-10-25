5f1b4a4a57d6baa7dd84dae1334dfb6d1eb3bb74
Razzwan <razvanlomov@gmail.com>
2018-10-25 16:35:15 +0300

    fix(convertSecondsToDuration): change logic of convertion

--- src/store/@common/helpers/covertSecondsToDuration.ts
+++ src/store/@common/helpers/covertSecondsToDuration.ts
@@ -2,10 +2,10 @@ import * as moment from 'moment';
 
 export function covertSecondsToDuration(seconds: number): string {
   const m = moment.utc(seconds * 1000);
-  // return m.format('HH:mm:ss');
-  if (seconds < 3600) {
-    return m.format('mm:ss') + ' мин';
-  } else {
-    return m.format('HH:mm') + ' ч';
-  }
+  return m.format('HH:mm:ss');
+  // if (seconds < 3600) {
+  //   return m.format('mm:ss') + ' мин';
+  // } else {
+  //   return m.format('HH:mm') + ' ч';
+  // }
 }
