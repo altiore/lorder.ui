1a51964e856b08665271dc1cfb118599050c8874
Razzwan <razvanlomov@gmail.com>
2019-01-03 13:56:26 +0200

    feat(chore): удалил devTools для продакшн версии

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -13,7 +13,9 @@ import { clientsMiddleware } from './@common/middlewares';
 import { createRootReducer } from './guestReducer';
 
 const composeEnhancers =
-  typeof window === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  process.env.NODE_ENV === 'development' &&
+  typeof window === 'object' &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
       })
