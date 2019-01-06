fe8e52d96162227f424197ae1c2e8b5bdfd09a9f
Razzwan <razvanlomov@gmail.com>
2019-01-06 05:40:41 +0200

    fix(users): fix main view for users

--- src/domains/main/@routes/users/Users.tsx
+++ src/domains/main/@routes/users/Users.tsx
@@ -14,6 +14,7 @@ import get from 'lodash-es/get';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
+import { Page } from 'src/components/Page';
 import Select from 'src/components/Select';
 import { LayoutLeftDrawer } from 'src/domains/@common/LayoutLeftDrawer';
 import { IUser } from 'src/store/users';
@@ -70,76 +71,78 @@ export class Users extends React.Component<RouteComponentProps<{}> & IUsersProps
     const { page, perPage } = this.state;
     return (
       <LayoutLeftDrawer>
-        {userList && userList.length ? (
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                <TableCell>Email</TableCell>
-                <TableCell>Телефон</TableCell>
-                <TableCell numeric>Статус</TableCell>
-                <TableCell className={classes.cell} numeric>
-                  Способ получения средств
-                </TableCell>
-                <TableCell className={classes.cell}>Роль</TableCell>
-                <TableCell style={{ width: 42 }} />
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {userList
-                .slice(page * perPage, (page + 1) * perPage)
-                .map(({ id, email, tel, status, paymentMethod, role }) => {
-                  return (
-                    <TableRow className={classes.row} key={id} hover onClick={this.handleRowClick(id)}>
-                      <TableCell component="th" scope="row">
-                        {email}
-                      </TableCell>
-                      <TableCell>{tel}</TableCell>
-                      <TableCell numeric>{status}</TableCell>
-                      <TableCell className={classes.cell} numeric>
-                        {paymentMethod}
-                      </TableCell>
-                      <TableCell className={classes.cell}>
-                        <Select
-                          autoWidth
-                          renderValue={this.renderSelectValue}
-                          IconComponent={this.renderEmpty}
-                          onChange={this.handleRoleChange(id)}
-                          value={role}
-                        >
-                          <MenuItem value={'user'}>User</MenuItem>
-                          <MenuItem value={'admin'}>Admin</MenuItem>
-                          <MenuItem value={'super-admin'}>Super-Admin</MenuItem>
-                        </Select>
-                      </TableCell>
-                      <TableCell>
-                        <IconButton onClick={this.handleRemoveClick(id)}>
-                          <ClearIcon />
-                        </IconButton>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  colSpan={3}
-                  count={userList.length}
-                  rowsPerPage={perPage}
-                  page={page}
-                  onChangePage={this.handleChangePage}
-                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                  labelRowsPerPage={'Элементов на странице'}
-                  labelDisplayedRows={this.labelDisplayedRows}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
-        ) : (
-          <Grid item xs={12}>
-            <img src={'/#'} />
-          </Grid>
-        )}
+        <Page>
+          {userList && userList.length ? (
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Телефон</TableCell>
+                  <TableCell numeric>Статус</TableCell>
+                  <TableCell className={classes.cell} numeric>
+                    Способ получения средств
+                  </TableCell>
+                  <TableCell className={classes.cell}>Роль</TableCell>
+                  <TableCell style={{ width: 42 }} />
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {userList
+                  .slice(page * perPage, (page + 1) * perPage)
+                  .map(({ id, email, tel, status, paymentMethod, role }) => {
+                    return (
+                      <TableRow className={classes.row} key={id} hover onClick={this.handleRowClick(id)}>
+                        <TableCell component="th" scope="row">
+                          {email}
+                        </TableCell>
+                        <TableCell>{tel}</TableCell>
+                        <TableCell numeric>{status}</TableCell>
+                        <TableCell className={classes.cell} numeric>
+                          {paymentMethod}
+                        </TableCell>
+                        <TableCell className={classes.cell}>
+                          <Select
+                            autoWidth
+                            renderValue={this.renderSelectValue}
+                            IconComponent={this.renderEmpty}
+                            onChange={this.handleRoleChange(id)}
+                            value={role}
+                          >
+                            <MenuItem value={'user'}>User</MenuItem>
+                            <MenuItem value={'admin'}>Admin</MenuItem>
+                            <MenuItem value={'super-admin'}>Super-Admin</MenuItem>
+                          </Select>
+                        </TableCell>
+                        <TableCell>
+                          <IconButton onClick={this.handleRemoveClick(id)}>
+                            <ClearIcon />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    colSpan={3}
+                    count={userList.length}
+                    rowsPerPage={perPage}
+                    page={page}
+                    onChangePage={this.handleChangePage}
+                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                    labelRowsPerPage={'Элементов на странице'}
+                    labelDisplayedRows={this.labelDisplayedRows}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          ) : (
+            <Grid item xs={12}>
+              <img src={'/#'} />
+            </Grid>
+          )}
+        </Page>
       </LayoutLeftDrawer>
     );
   }
