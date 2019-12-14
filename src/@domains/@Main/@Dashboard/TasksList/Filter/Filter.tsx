8eef907346219307308ddc622bf2c5636e11fa7d
Razzwan <razvanlomov@gmail.com>
2019-12-14 18:31:03 +0200

    AL-2435: отображение фильтров на главной

--- src/@domains/@Main/@Dashboard/TasksList/Filter/Filter.tsx
+++ src/@domains/@Main/@Dashboard/TasksList/Filter/Filter.tsx
@@ -1,3 +1,5 @@
+import React, { useCallback, useMemo, useState } from 'react';
+
 import Chip from '@material-ui/core/Chip';
 import Fab from '@material-ui/core/Fab';
 import Grow from '@material-ui/core/Grow';
@@ -8,7 +10,6 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import DoneIcon from '@material-ui/icons/Done';
 import get from 'lodash-es/get';
-import React from 'react';
 
 type FilterType = 'smart' | 'recent' | 'new';
 
@@ -31,113 +32,107 @@ export interface IFilterState {
 const FILTERS = { smart: 'Ценные', recent: 'Недавние', new: 'Новые' };
 const getLabelFromFilter = (filter: FilterType) => FILTERS[filter];
 
-export class FilterTsx extends React.PureComponent<IFilterProps, IFilterState> {
-  state = {
-    hoveredFilter: undefined,
-    isFilterHovered: false,
-    isPaginatorHovered: false,
-  };
+export const FilterTsx: React.FC<IFilterProps> = ({
+  classes,
+  changeTasksFilter,
+  page,
+  count,
+  perPage,
+  changePage,
+  filter,
+}) => {
+  const [hoveredFilter, setHoveredFilter] = useState();
+  const handleRadioEnter = useCallback((e: React.SyntheticEvent) => {
+    setHoveredFilter(get(e, 'target.value'));
+  }, []);
+  const handleRadioLeave = useCallback(() => setHoveredFilter(undefined), []);
 
-  render() {
-    const { classes, page, count, perPage, changePage, filter } = this.props;
-    const { hoveredFilter, isFilterHovered, isPaginatorHovered } = this.state;
+  const [isFilterHovered, setIsFilterHovered] = useState(false);
+  const toggleFilterHovered = useCallback(() => {
+    setIsFilterHovered(state => !state);
+  }, []);
+  const handleSetHovered = useCallback(() => setIsFilterHovered(true), []);
+  const handleSetNotHovered = useCallback(() => setIsFilterHovered(false), []);
 
-    return (
-      <ListItem className={classes.root}>
-        <div
-          className={classes.pagination}
-          onMouseEnter={this.handlePaginatorEnter}
-          onMouseLeave={this.handlePaginatorLeave}
-          onFocus={this.handlePaginatorEnter}
-          onBlur={this.handlePaginatorLeave}
-        >
-          <Grow in={isPaginatorHovered} timeout={600}>
-            <Fab size="small" color="secondary" onClick={changePage(page - 1)} className={classes.left}>
-              <ChevronLeftIcon fontSize="small" />
-            </Fab>
+  const [isPaginatorHovered, setIsPaginatorHovered] = useState(false);
+  const handlePaginatorEnter = useCallback(() => setIsPaginatorHovered(true), []);
+  const handlePaginatorLeave = useCallback(() => setIsPaginatorHovered(false), []);
+
+  const currentSelectedFilter = useMemo(() => {
+    return FILTERS[hoveredFilter || filter];
+  }, [hoveredFilter, filter]);
+
+  const handleChangeTaskFilter = useCallback(
+    (e: React.ChangeEvent<any>) => {
+      const newFilter = get(e, ['target', 'value']);
+      changeTasksFilter(newFilter);
+    },
+    [changeTasksFilter]
+  );
+
+  return (
+    <ListItem className={classes.root}>
+      <div
+        className={classes.pagination}
+        onMouseEnter={handlePaginatorEnter}
+        onMouseLeave={handlePaginatorLeave}
+        onFocus={handlePaginatorEnter}
+        onBlur={handlePaginatorLeave}
+      >
+        <Grow in={isPaginatorHovered} timeout={600}>
+          <Fab size="small" color="secondary" onClick={changePage(page - 1)} className={classes.left}>
+            <ChevronLeftIcon fontSize="small" />
+          </Fab>
+        </Grow>
+        {page + 1} из {Math.ceil(count / perPage)}
+        <Grow in={isPaginatorHovered} timeout={600}>
+          <Fab size="small" color="secondary" onClick={changePage(page + 1)} className={classes.right}>
+            <ChevronRightIcon fontSize="small" />
+          </Fab>
+        </Grow>
+      </div>
+      <div className={classes.grow} />
+      <div className={classes.filter} onMouseEnter={handleSetHovered} onMouseLeave={handleSetNotHovered}>
+        <Chip
+          label={currentSelectedFilter}
+          clickable
+          className={classes.chip}
+          color="secondary"
+          deleteIcon={<DoneIcon />}
+          onClick={toggleFilterHovered}
+          style={{ bottom: isFilterHovered ? 40 : 16 }}
+          onFocus={handleSetHovered}
+          onBlur={handleSetNotHovered}
+        />
+        {isFilterHovered && (
+          <Grow in={isFilterHovered} timeout={500}>
+            <RadioGroup
+              row
+              aria-label="Gender"
+              name="filter"
+              className={classes.group}
+              value={filter}
+              onChange={handleChangeTaskFilter}
+            >
+              {Object.keys(FILTERS).map((f: any) => {
+                const label = getLabelFromFilter(f);
+                return (
+                  <Radio
+                    key={f}
+                    color="primary"
+                    checked={filter === f}
+                    value={f}
+                    name="task-filter"
+                    aria-label={label}
+                    onMouseEnter={handleRadioEnter}
+                    onMouseLeave={handleRadioLeave}
+                  />
+                );
+              })}
+            </RadioGroup>
           </Grow>
-          {page + 1} из {Math.ceil(count / perPage)}
-          <Grow in={isPaginatorHovered} timeout={600}>
-            <Fab size="small" color="secondary" onClick={changePage(page + 1)} className={classes.right}>
-              <ChevronRightIcon fontSize="small" />
-            </Fab>
-          </Grow>
-        </div>
-        <div className={classes.grow} />
-        <div className={classes.filter} onMouseEnter={this.handleFilterEnter} onMouseLeave={this.handleFilterLeave}>
-          <Chip
-            label={getLabelFromFilter(hoveredFilter || filter)}
-            clickable
-            className={classes.chip}
-            color="secondary"
-            deleteIcon={<DoneIcon />}
-            onClick={this.toggleFilterHovered(!this.state.isFilterHovered)}
-            style={{ bottom: isFilterHovered ? 40 : 16 }}
-            onFocus={this.toggleFilterHovered(true)}
-            onBlur={this.toggleFilterHovered(false)}
-          />
-          {isFilterHovered && (
-            <Grow in={isFilterHovered} timeout={500}>
-              <RadioGroup
-                row
-                aria-label="Gender"
-                name="filter"
-                className={classes.group}
-                value={filter}
-                onChange={this.changeTaskFilter as any}
-              >
-                {Object.keys(FILTERS).map((f: any) => {
-                  const label = getLabelFromFilter(f);
-                  return (
-                    <Radio
-                      key={f}
-                      color="secondary"
-                      checked={filter === f}
-                      value={f}
-                      name="task-filter"
-                      aria-label={label}
-                      onMouseEnter={this.handleRadioEnter}
-                      onMouseLeave={this.handleRadioLeave}
-                    />
-                  );
-                })}
-              </RadioGroup>
-            </Grow>
-          )}
-        </div>
-      </ListItem>
-    );
-  }
-
-  private changeTaskFilter = (e: React.SyntheticEvent) => {
-    this.props.changeTasksFilter(e);
-  };
-
-  private handleFilterEnter = () => {
-    this.setState({ isFilterHovered: true });
-  };
-
-  private handleFilterLeave = () => {
-    this.setState({ isFilterHovered: false });
-  };
-
-  private toggleFilterHovered = (isFilterHovered: boolean) => () => {
-    this.setState({ isFilterHovered });
-  };
-
-  private handleRadioEnter = (e: React.SyntheticEvent) => {
-    this.setState({ hoveredFilter: get(e, 'target.value') });
-  };
-
-  private handleRadioLeave = () => {
-    this.setState({ hoveredFilter: undefined });
-  };
-
-  private handlePaginatorEnter = () => {
-    this.setState({ isPaginatorHovered: true });
-  };
-
-  private handlePaginatorLeave = () => {
-    this.setState({ isPaginatorHovered: false });
-  };
-}
+        )}
+      </div>
+    </ListItem>
+  );
+};
