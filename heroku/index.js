cc278dbbb9284428fb603550ac784f4659c45e84
Razzwan <razvanlomov@gmail.com>
2019-12-26 11:32:53 +0200

    исправил index.js файл для сервера

--- heroku/index.js
+++ heroku/index.js
@@ -3,8 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5005;
 
 express()
-  .use(express.static(path.join(__dirname, '.')))
+  .use(express.static(path.join(__dirname, 'build')))
   .get('/*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/build/index.html');
   })
   .listen(PORT);
