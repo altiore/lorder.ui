56bcb8beeb3b6406e746f3405a78bc7f8b41b301
Razzwan <razvanlomov@gmail.com>
2019-12-29 14:47:02 +0200

    удалил установки cookies из FE server-а

--- heroku/index.js
+++ heroku/index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 5005;
 express()
   .use(express.static(path.join(__dirname, 'build')))
   .get('/*', function(req, res) {
-    res.setHeader('Set-Cookie', 'HttpOnly;Secure;SameSite=Strict');
     res.sendFile(__dirname + '/build/index.html');
   })
   .listen(PORT);
