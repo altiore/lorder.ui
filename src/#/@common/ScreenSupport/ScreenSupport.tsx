76ff8d1b11c01808a852ff71e46bdce3dcb23021
srs <artaksergievich@gmail.com>
2020-05-11 01:37:48 +0300

    AL-751: added support page

--- src/#/@common/ScreenSupport/ScreenSupport.tsx
+++ src/#/@common/ScreenSupport/ScreenSupport.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, Theme } from '@material-ui/core/styles';
+<<<<<<< HEAD:src/#/@common/ScreenSupport/ScreenSupport.tsx
 
+=======
+>>>>>>> 4da359d... AL-751: added support page:src/#/@common/ScreenSupport/ScreenSupport.tsx
 import Block from '#/#hi/#/@common/Block';
 import ScreenTitle from '#/#hi/#/@common/ScreenTitle';
 
