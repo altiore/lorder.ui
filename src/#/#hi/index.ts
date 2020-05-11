b0ac5b50e24e3c096328365dff8d7985fa3386c1
srs <maniman91job@yandex.ru>
2020-05-12 00:01:38 +0300

    AL-751: добавил страницу поддержки
    результаты второго урока

--- src/#/#hi/index.ts
+++ src/#/#hi/index.ts
@@ -1,17 +1,24 @@
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
 
 import { withStyles } from '@material-ui/core/styles';
 
 import { fetchAltiore } from '#/@store/publicAltiore';
 import { fetchStatistics } from '#/@store/statistics';
+import { userRole } from '#/@store/identity';
+
 
 import { HiTsx } from './hi-layout';
 import { styles } from './styles';
 
 import { withResize } from '@hooks/withResize';
 
-const masStateToProps = () => ({
-  brandName: 'Altiore',
+
+const brandName = () => 'Altiore'
+
+const mapStateToProps = createStructuredSelector({
+  brandName,
+  userRole
 });
 
 const mapDispatch = {
@@ -20,6 +27,6 @@ const mapDispatch = {
 };
 
 export default connect(
-  masStateToProps,
+  mapStateToProps,
   mapDispatch
 )(withStyles(styles)(withResize(HiTsx)) as any);
