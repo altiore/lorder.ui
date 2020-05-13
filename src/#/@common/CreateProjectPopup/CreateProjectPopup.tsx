30323c0409d24c0310494c7ddbf33a479b9b1260
srs ad <maniman91job@yandex.ru>
2020-05-13 23:44:54 +0300

    AL-784: добавлен параметр 'type' в форму создания проекта

--- src/#/@common/CreateProjectPopup/CreateProjectPopup.tsx
+++ src/#/@common/CreateProjectPopup/CreateProjectPopup.tsx
@@ -4,22 +4,30 @@ import Button from '@material-ui/core/Button';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 
+import { PROJECT_TYPE } from '@types';
 import { TextField } from '@components/TextField';
+import { SwitchField } from '@components/SwitchField'
 
 import { Field, InjectedFormProps } from 'redux-form';
 import { required } from 'redux-form-validators';
 
-export class IProjectFormProps {
+import { useStyles } from './styles';
+
+export class IProjectFormProps{
   goToPage: any;
   onClose: any;
   title?: string;
+  type?: string;
   buttonText?: string;
 }
 
+
 export const CreateProjectPopupJsx: React.FunctionComponent<
   IProjectFormProps & InjectedFormProps<{}, IProjectFormProps>
-> = ({ handleSubmit, onClose }) => (
-  <React.Fragment>
+> = ({ handleSubmit, onClose }) => {
+  const classes = useStyles();
+
+  return (<React.Fragment>
     <DialogContent>
       <form onSubmit={handleSubmit}>
         <Field
@@ -31,15 +39,24 @@ export const CreateProjectPopupJsx: React.FunctionComponent<
           label="Название проекта"
           validate={[required({ msg: 'Обязательное поле' })]}
         />
+        <div className={classes.textRight}>
+          <Field
+            name="type"
+            component={SwitchField}
+            label="Личный"
+            on={PROJECT_TYPE.PERSONALLY_USEFUL}
+            off={PROJECT_TYPE.SOCIALLY_USEFUL}
+          />
+        </div>
       </form>
     </DialogContent>
     <DialogActions>
       <Button color="primary" onClick={onClose}>
-        Cancel
+        Отмена
       </Button>
       <Button color="primary" onClick={handleSubmit}>
         Создать проект
       </Button>
     </DialogActions>
   </React.Fragment>
-);
+)};
