3f175265c5af7ad3b141a4f3ce46b26c59c28a5d
Razzwan <razvanlomov@gmail.com>
2020-05-18 19:37:39 +0300

    AL-835: add Form decorator

---
+++ src/@components/TextField/TextField.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Field } from 'redux-form';
+
+import Button from '@material-ui/core/Button';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import { SelectField } from '@components/SelectField';
+
+import { TextField } from '.';
+import FormDecorator from '../../../.storybook/decor/FormDecorator';
+
+import { storiesOf } from '@storybook/react';
+
+enum YesOrNo {
+  YES = 'да',
+  NO = 'нет',
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  form: {
+    width: 320,
+  },
+  root: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    minHeight: 200,
+    padding: theme.spacing(2),
+  },
+}));
+
+storiesOf('TextField', module)
+  .addDecorator(FormDecorator)
+  .add('default', () => {
+    const { form, root } = useStyles();
+    return (
+      <div className={root}>
+        <div className={form}>
+          <Field name="test" component={TextField} />
+          <Field name="select" component={SelectField} items={YesOrNo} />
+          <Button type="submit">Отправить</Button>
+        </div>
+      </div>
+    );
+  });
