60e769c4b2f6c2304ac80561d6ff29838d019770
Razzwan <razvanlomov@gmail.com>
2020-05-18 19:37:39 +0300

    AL-835: add Form decorator

--- .storybook/decor/FormDecorator.js
+++ .storybook/decor/FormDecorator.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
-import { reducer } from 'redux-form';
+import { reducer, reduxForm } from 'redux-form';
+import { action } from '@storybook/addon-actions';
 
 const mockStore = createStore(combineReducers({ form: reducer }));
 
-const FormDecorator = getStory => <Provider store={mockStore}>{getStory()}</Provider>;
+const MyForm = ({ handleChange, handleSubmit, getStory }) => {
+  return (
+    <form onSubmit={handleSubmit} onChange={handleChange}>
+      {getStory()}
+    </form>
+  );
+};
+
+const Form = reduxForm({
+  form: 'TestForm',
+})(MyForm);
+
+const FormDecorator = getStory => {
+  return (
+    <Provider store={mockStore}>
+      <Form getStory={getStory} onSubmit={action('Submit')} />
+    </Provider>
+  );
+};
 
 export default FormDecorator;
