c3ea9e1fe261defb4e9abfcd045d6e196e658348
Razzwan <razvanlomov@gmail.com>
2020-05-28 13:56:39 +0300

    AL-860: update packages and remove console bugs

--- src/#/@common/Notification/Notification.tsx
+++ src/#/@common/Notification/Notification.tsx
@@ -1,73 +1,55 @@
-// import green from '@material-ui/core/colors/green'
-import React from 'react';
-import NotifySystem from 'react-notification-system';
+import React, { useEffect, useRef } from 'react';
+import NotifySystem, { System } from 'react-notification-system';
 
 export interface INotificationProps {
   hide: any;
   notifications: any;
 }
 
-export class Notification extends React.Component<INotificationProps, {}> {
-  notification: React.RefObject<any>;
+export const Notification: React.FC<INotificationProps> = props => {
+  const { hide, notifications } = props;
+  const notification = useRef<System>(null);
 
-  constructor(props: INotificationProps) {
-    super(props);
+  useEffect(() => {
+    const curNotification = notification.current;
+    if (curNotification) {
+      const notificationIds = notifications.map((notification: any) => notification.uid);
+      const systemNotifications = curNotification.state.notifications || [];
 
-    this.notification = React.createRef();
-  }
-
-  componentWillReceiveProps(nextProps: INotificationProps) {
-    const { notifications } = nextProps;
-    const notificationIds = notifications.map((notification: any) => notification.uid);
-    const systemNotifications = this.notification.current.state.notifications || [];
-
-    if (notifications.length > 0) {
-      // Get all active notifications from react-notification-system
-      /// and remove all where uid is not found in the reducer
-      systemNotifications.forEach((notification: any) => {
-        if (notificationIds.indexOf(notification.uid) < 0) {
-          this.notification.current.removeNotification(notification.uid);
-        }
-      });
-
-      notifications.forEach((notification: any) => {
-        this.notification.current.addNotification({
-          ...notification,
-          onRemove: () => {
-            this.props.hide(notification.uid);
-            if (notification.onRemove) {
-              notification.onRemove(notification);
-            }
-          },
+      if (notifications.length > 0) {
+        // Get all active notifications from react-notification-system
+        // and remove all where uid is not found in the reducer
+        systemNotifications.forEach((notification: any) => {
+          if (notificationIds.indexOf(notification.uid) < 0) {
+            notification.current.removeNotification(notification.uid);
+          }
         });
-      });
+
+        notifications.forEach((notification: any) => {
+          curNotification.addNotification({
+            ...notification,
+            onRemove: () => {
+              hide(notification.uid);
+              if (notification.onRemove) {
+                notification.onRemove();
+              }
+            },
+          });
+        });
+      }
     }
+  }, [hide, notification, notifications]);
 
-    if (this.props.notifications !== notifications && notifications.length === 0) {
-      this.notification.current.clearNotifications();
-    }
-  }
-
-  shouldComponentUpdate(nextProps: INotificationProps) {
-    return this.props !== nextProps;
-  }
-
-  render() {
-    // Optional styling
-    const style = {
-      NotificationItem: {
-        // Override the notification item
-        DefaultStyle: {
-          // Applied to every notification, regardless of the notification level
-          margin: '10px 5px 2px 1px',
-        },
-
-        // success: { // Applied only to the success notification item
-        //   color: green.A400,
-        // },
+  // Optional styling
+  const style = {
+    NotificationItem: {
+      // Override the notification item
+      DefaultStyle: {
+        // Applied to every notification, regardless of the notification level
+        margin: '10px 5px 2px 1px',
       },
-    };
+    },
+  };
 
-    return <NotifySystem ref={this.notification} style={style} {...this.props} />;
-  }
-}
+  return <NotifySystem ref={notification} style={style} {...props} />;
+};
--- src/@components/TimeLine/TimeLine.stories.tsx
+++ src/@components/TimeLine/TimeLine.stories.tsx
@@ -4,35 +4,46 @@ import { TimeLine } from '.';
 
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
+import { IEvent, ITask, IUserWork } from '@types';
 
 const moment = require('moment');
 
 const startJob = moment().subtract(5, 'hours');
 const startJob3 = moment().subtract(1, 'hours');
 
-const events = [
+const events: IEvent[] = [
   {
-    data: 1,
-    finishAt: startJob,
+    userWork: {
+      finishAt: startJob,
+      startAt: moment()
+        .subtract(1, 'day')
+        .set('hours', 23),
+    } as IUserWork,
+
+    task: {} as ITask,
+
     isActive: false,
     name: 'Сон',
-    startAt: moment()
-      .subtract(1, 'day')
-      .set('hours', 23),
   },
   {
-    data: 2,
-    finishAt: startJob3,
+    userWork: {
+      finishAt: startJob3,
+      startAt: startJob,
+    } as IUserWork,
+
+    task: {} as ITask,
+
     isActive: true,
     name: 'Работа',
-    startAt: startJob,
   },
   {
-    data: 3,
-    finishAt: null,
     isActive: true,
     name: 'Работа вторая',
-    startAt: startJob3,
+    task: {} as ITask,
+    userWork: {
+      finishAt: null,
+      startAt: startJob3,
+    } as IUserWork,
   },
 ];
 
