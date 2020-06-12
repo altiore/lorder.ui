a33e3835d9fa677f828f3468647f5e35ad6d1f3f
Razzwan <razvanlomov@gmail.com>
2020-06-12 11:16:28 +0300

    LR-XXX: исправил критический баг

--- src/#/@store/tasks/thunk/fetchProjectTasks.ts
+++ src/#/@store/tasks/thunk/fetchProjectTasks.ts
@@ -1,5 +1,3 @@
-import get from 'lodash/get';
-
 import { fetchProjectTasksA } from '../actions';
 import { projectTasks } from '../selectorsCombined';
 
@@ -7,8 +5,8 @@ const PER_PAGE_COUNT = 100;
 
 export const fetchProjectTasks = (projectId, skip = 0, force: boolean = false) => async (dispatch, getState) => {
   const res = await dispatch(fetchProjectTasksA({ projectId, skip, count: PER_PAGE_COUNT }, force));
-  if (projectTasks(getState()).length < get(res, ['payload', 'data', 'total'])) {
-    if (!get(res, ['payload', 'data', 'list', 'length'])) {
+  if (projectTasks(getState()).length < res?.payload?.data?.total) {
+    if (!res?.payload?.data?.data?.length) {
       console.warn(
         'В ответе сервера нет данных, но данные получены не все. Нужно обработать случай, когда задача появляется новая задача, добавленная кем-то другим!'
       );
