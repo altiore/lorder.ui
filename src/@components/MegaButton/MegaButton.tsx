8b81525af6ea60838816b025eed32af70af7d1ba
LoginovSO <LoginovSO@yandex.ru>
2020-06-15 23:24:17 +0300

    AL-898: Add trasintion + fix + styles

--- src/@components/MegaButton/MegaButton.tsx
+++ src/@components/MegaButton/MegaButton.tsx
@@ -1,8 +1,15 @@
 import React, { useCallback, useState } from 'react';
 
+import cn from 'classnames';
+
 import ButtonBase from '@material-ui/core/ButtonBase';
+import Fade from '@material-ui/core/Fade';
 import Popper from '@material-ui/core/Popper';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import { ReactComponent as BackSvg } from './back.svg';
+import { ReactComponent as CheckSvg } from './check.svg';
+import { ReactComponent as PlaySvg } from './play.svg';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -10,7 +17,7 @@ const useStyles = makeStyles((theme: Theme) =>
       '&:hover': {
         cursor: 'pointer',
       },
-      background: '#8c6daf',
+      background: theme.palette.success.main,
       border: 'none',
       borderRadius: '50%',
       boxShadow: '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)',
@@ -19,7 +26,7 @@ const useStyles = makeStyles((theme: Theme) =>
       zIndex: 100,
     },
     leftButtonTag: {
-      background: '#ef5d74',
+      background: theme.palette.error.main,
     },
     megaButton: {
       display: 'flex',
@@ -29,7 +36,7 @@ const useStyles = makeStyles((theme: Theme) =>
       '&:hover': {
         cursor: 'pointer',
       },
-      background: '#77af6d',
+      // background: '#77af6d',
       border: 'none',
       borderRadius: '50%',
       boxShadow: '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)',
@@ -51,7 +58,15 @@ const useStyles = makeStyles((theme: Theme) =>
       width: '140px',
     }),
     rightButtonTag: {
-      background: '#8cc759',
+      background: theme.palette.success.main,
+    },
+    svgCentralButtonTag: {
+      fill: 'white',
+      width: '20px',
+    },
+    svgOptionalButtonTag: {
+      fill: 'white',
+      width: '20px',
     },
   })
 );
@@ -81,7 +96,16 @@ export const MegaButton: React.FC<Props> = ({ onClickLeft, onClickCenter, onClic
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [visible, setVisible] = React.useState(false);
 
-  const { megaButton, optionalButtonTag, centralButtonTag, paper } = useStyles({
+  const {
+    megaButton,
+    optionalButtonTag,
+    centralButtonTag,
+    paper,
+    svgCentralButtonTag,
+    svgOptionalButtonTag,
+    leftButtonTag,
+    rightButtonTag,
+  } = useStyles({
     visible,
   });
 
@@ -94,31 +118,38 @@ export const MegaButton: React.FC<Props> = ({ onClickLeft, onClickCenter, onClic
     setAnchorEl(null);
   };
 
-  // const open = Boolean(anchorEl);
-  const open = true;
-  // const id = open ? 'simple-popper' : undefined;
-  const id = 'simple-popper';
+  const open = Boolean(anchorEl);
+  const checked = open;
+  // const open = true;
+  const id = open ? 'megaButton' : undefined;
+  // const id = 'simple-popper';
 
   return (
     <>
       <div className={megaButton}>
         <ButtonBase onMouseOver={handleOnMouseOver2} onClick={clickCenter} className={centralButtonTag}>
-          <p>ЦК</p>
+          <PlaySvg className={svgCentralButtonTag} />
         </ButtonBase>
-
-        <Popper id={id} open={open} anchorEl={anchorEl}>
-          <div className={paper} onMouseLeave={handleOnMouseLeave}>
-            {typeof onClickLeft === 'function' ? (
-              <ButtonBase className={optionalButtonTag} onClick={clickLeft}>
-                <p>ЛК</p>
-              </ButtonBase>
-            ) : null}
-            {typeof onClickRight === 'function' ? (
-              <ButtonBase className={optionalButtonTag} onClick={clickRight}>
-                <p>ПК</p>
-              </ButtonBase>
-            ) : null}
-          </div>
+        <Popper id={id} open={open} anchorEl={anchorEl} role={undefined} transition>
+          {({ TransitionProps, placement }) => (
+            <Fade
+              {...TransitionProps}
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <div className={paper} onMouseLeave={handleOnMouseLeave}>
+                {typeof onClickLeft === 'function' ? (
+                  <ButtonBase className={cn(optionalButtonTag, leftButtonTag)} onClick={clickLeft}>
+                    <BackSvg className={svgOptionalButtonTag} />
+                  </ButtonBase>
+                ) : null}
+                {typeof onClickRight === 'function' ? (
+                  <ButtonBase className={cn(optionalButtonTag, rightButtonTag)} onClick={clickRight}>
+                    <CheckSvg className={svgOptionalButtonTag} />
+                  </ButtonBase>
+                ) : null}
+              </div>
+            </Fade>
+          )}
         </Popper>
       </div>
     </>
