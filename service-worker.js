2e4e24622e4edf09bbb323b9c3c32eb378bf8fbd
Razzwan <razvanlomov@gmail.com>
2020-07-06 13:43:06 +0000

    Deploying to gh-pages from  @ 828c66eae1605737a3cbf18a0ea86e418ffe7b7c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.372d4c913bc8d43419b39e4941ba36c2.js"
+  "precache-manifest.ec3766c9017ad3b382ca84a79201cbd5.js"
 );
 
 self.addEventListener('message', (event) => {
