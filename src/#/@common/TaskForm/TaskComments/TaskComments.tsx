30acb0c79ff2a0d5fe93173fc04acec6e10d6c16
rastym <rostislav0022@gmail.com>
2020-07-11 14:34:43 +0300

    LR-974: Комментарии к задачам.Итерация №2

--- src/#/@common/TaskForm/TaskComments/TaskComments.tsx
+++ src/#/@common/TaskForm/TaskComments/TaskComments.tsx
@@ -11,9 +11,32 @@ import {
   Typography,
 } from '@material-ui/core';
 import { TextField } from '@material-ui/core/';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { Delete as DeleteIcon } from '@material-ui/icons';
 
 import { useStyles } from './style';
+
+import { IUser } from '@types';
+
+interface ITaskCommentsProps {
+  addTaskComment: (projectId: number, taskId: number, comment: string) => Promise<IComment>;
+  userId: number;
+  removeTaskComment: (projectId: number, taskId: number, commentId: number) => Promise<number>;
+  sequenceNumber: number;
+  getTaskIdBySequenceNumber: (sequenceNumber: number, projectId: number) => number;
+  projectId: number;
+  fetchTaskComments: (projectId: number, taskId: number) => Promise<IComment[]>;
+}
+
+interface IComment {
+  createdAt: string;
+  id: number;
+  taskId: number;
+  text: string;
+  updatedAt: string;
+  user: IUser;
+  userId: number;
+}
 
 export const TaskComments = ({
   addTaskComment,
@@ -23,21 +46,34 @@ export const TaskComments = ({
   getTaskIdBySequenceNumber,
   projectId,
   fetchTaskComments,
-}: any) => {
+}: ITaskCommentsProps) => {
   const [currentSequenceNumber] = useState(sequenceNumber);
   const [taskComments, setTaskComments] = useState([]);
   const [commentInput, setCommentInput] = useState('');
+  const [isFetching, setIsFetching] = useState(true);
+  const [isCommentAdd, setIsCommentAdd] = useState(false);
+  const [isCommentRemove, setIsCommentRemove] = useState(false);
+  const [removedCommentId, setRemovedCommentId] = useState(-1);
 
   useEffect(() => {
     if (getTaskIdBySequenceNumber(currentSequenceNumber, projectId)) {
-      fetchTaskComments(projectId, getTaskIdBySequenceNumber(currentSequenceNumber, projectId)).then(setTaskComments);
+      fetchTaskComments(projectId, getTaskIdBySequenceNumber(currentSequenceNumber, projectId)).then(
+        (comments: IComment[]) => {
+          setIsFetching(false);
+          setTaskComments(comments as any);
+        }
+      );
     }
-  }, [currentSequenceNumber, fetchTaskComments, getTaskIdBySequenceNumber, projectId]);
+  }, [currentSequenceNumber, fetchTaskComments, getTaskIdBySequenceNumber, projectId, isFetching]);
 
   const removeComment = commentId => () => {
+    setIsCommentRemove(true);
+    setRemovedCommentId(commentId);
     removeTaskComment(projectId, getTaskIdBySequenceNumber(currentSequenceNumber, projectId), commentId).then(
-      removedCommentId => {
-        setTaskComments(taskComments.filter((comm: any) => comm.id !== removedCommentId));
+      removedId => {
+        setTaskComments(taskComments.filter((comm: IComment) => comm.id !== removedId));
+        setRemovedCommentId(-1);
+        setIsCommentRemove(false);
       }
     );
   };
@@ -46,50 +82,72 @@ export const TaskComments = ({
   const handleInput = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
     setCommentInput(value);
   };
-
   const handleAddTaskComment = useCallback(() => {
     if (getTaskIdBySequenceNumber(currentSequenceNumber, projectId)) {
+      setIsCommentAdd(true);
       addTaskComment(projectId, getTaskIdBySequenceNumber(currentSequenceNumber, projectId), commentInput).then(
         newComment => {
           setTaskComments([...taskComments, newComment] as any);
           setCommentInput('');
+          setIsCommentAdd(false);
         }
       );
     }
   }, [currentSequenceNumber, projectId, getTaskIdBySequenceNumber, addTaskComment, taskComments, commentInput]);
   if (getTaskIdBySequenceNumber(currentSequenceNumber, projectId)) {
     return (
-      <div style={{ maxWidth: '75%' }}>
+      <div className={classes.commentsWrap}>
         <h2 style={{ textAlign: 'center' }}>Комментарии</h2>
         <Divider />
-        {taskComments.map((comment: any) => (
-          <React.Fragment key={comment.id}>
-            <ListItem alignItems="flex-start" style={{ position: 'relative' }}>
-              <ListItemAvatar>
-                <Avatar alt="Remy Sharp" src={comment.user.avatar.url || process.env.PUBLIC_URL + '/d-avatar.png'} />
-              </ListItemAvatar>
-              <ListItemText
-                primary={comment.user.displayName || 'N/A'}
-                secondary={
-                  <React.Fragment>
-                    <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
-                      {comment.text}
-                    </Typography>
-                    {userId === comment.user.id && (
-                      <span style={{ position: 'absolute', right: 4, top: 4 }}>
-                        <Tooltip title="Удалить комментарий" placement="left-start">
-                          <DeleteIcon className={classes.basketIcon} onClick={removeComment(comment.id)} />
-                        </Tooltip>
-                      </span>
-                    )}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-            <Divider />
-          </React.Fragment>
-        ))}
-        <div style={{ marginTop: 20, marginBottom: 20, background: 'transparent' }}>
+        {isFetching && !taskComments.length ? (
+          <div className={classes.circularWrap}>
+            <CircularProgress />
+          </div>
+        ) : (
+          taskComments.map((comment: IComment) => {
+            return (
+              <React.Fragment key={comment.id}>
+                <ListItem alignItems="flex-start" style={{ position: 'relative' }}>
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src={comment.user.avatar?.url || process.env.PUBLIC_URL + '/d-avatar.png'}
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={comment.user.displayName || 'N/A'}
+                    secondary={
+                      <React.Fragment>
+                        <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
+                          {comment.text}
+                        </Typography>
+                        {userId === comment.user.id && (
+                          <span className={classes.basketWrap}>
+                            <Tooltip title="Удалить комментарий" placement="top-end">
+                              <DeleteIcon className={classes.basketIcon} onClick={removeComment(comment.id)} />
+                            </Tooltip>
+                          </span>
+                        )}
+                        {isCommentRemove && comment.id === removedCommentId && (
+                          <div className={classes.circularRemoveWrap}>
+                            <CircularProgress />
+                          </div>
+                        )}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Divider />
+              </React.Fragment>
+            );
+          })
+        )}
+        {isCommentAdd && taskComments.length ? (
+          <div className={classes.circularWrap}>
+            <CircularProgress />
+          </div>
+        ) : null}
+        <div className={classes.textareaWrap}>
           <TextField
             id="outlined-multiline-static"
             label="Оставьте комментарий"
@@ -100,9 +158,9 @@ export const TaskComments = ({
             value={commentInput}
           />
           <Button
-            style={{ marginTop: 20, marginBottom: 20 }}
             variant="contained"
             color="primary"
+            className={classes.addButton}
             onClick={handleAddTaskComment as any}
             disabled={!Boolean(commentInput.trim().length)}
           >
