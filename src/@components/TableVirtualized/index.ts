c633b769d691e19d89035d7078db946c9319b8da
Razzwan <razvanlomov@gmail.com>
2020-07-31 12:52:34 +0300

    LR-XXX: remove useless library (react-virtualized)

--- src/@components/TableVirtualized/index.ts
+++
@@ -1,7 +0,0 @@
-import { withStyles } from '@material-ui/core/styles';
-
-import { styles } from './styles';
-import { ColumnType as ColumnTypeRaw, TableVirtualized } from './TableVirtualized';
-
-export default withStyles(styles, { withTheme: true })(TableVirtualized);
-export type ColumnType = ColumnTypeRaw;
