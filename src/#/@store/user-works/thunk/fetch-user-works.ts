daa1134f775a58807440ba6d5b3edc5d6578c626
Razzwan <razvanlomov@gmail.com>
2020-09-15 22:46:35 +0300

    LR-1082: Исправить проблему с пустыми данными на доске пользователя
    LR-1082: исправил проблему с пустыми данными при инициализации приложения

--- src/#/@store/user-works/thunk/fetch-user-works.ts
+++ src/#/@store/user-works/thunk/fetch-user-works.ts
@@ -5,14 +5,21 @@ import { UserWork } from '../UserWork';
 import { startTimer, stopUserWork } from './actionUserWork';
 
 export const fetchUserWorks = () => async (dispatch: any, getState: any) => {
-  const res = await dispatch(getUserWorksAct());
-  const userWorkList = res?.payload?.data;
-  const currentUserWork: Partial<UserWork> | undefined = userWorkList.find((userWork: UserWork) => !userWork.finishAt);
-  if (currentUserWork) {
-    dispatch(startTimer(currentUserWork));
-  } else {
-    if (isTimerStarted(getState())) {
-      dispatch(stopUserWork());
+  try {
+    const res = await dispatch(getUserWorksAct());
+    const userWorkList = res?.payload?.data;
+    const currentUserWork: Partial<UserWork> | undefined =
+      userWorkList && userWorkList.find((userWork: UserWork) => !userWork.finishAt);
+    if (currentUserWork) {
+      dispatch(startTimer(currentUserWork));
+    } else {
+      if (isTimerStarted(getState())) {
+        dispatch(stopUserWork());
+      }
+    }
+  } catch (e) {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(e);
     }
   }
 };
