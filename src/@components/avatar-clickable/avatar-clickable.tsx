466e295e9f76a3d5ae2c06cbc6d15bd260f3f84f
jolyfan <c0d3ar@ya.ru>
2021-01-08 16:24:31 +0500

    rev: removed unused props, div replaced to fragment

--- src/@components/avatar-clickable/avatar-clickable.tsx
+++ src/@components/avatar-clickable/avatar-clickable.tsx
@@ -4,17 +4,14 @@ import Avatar from '@material-ui/core/Avatar';
 import ButtonBase from '@material-ui/core/ButtonBase';
 
 export interface IAvatarProps {
-  children?: string | number;
-  isSelected?: boolean;
   onClick?: (e: React.SyntheticEvent) => any;
-  size?: 'sm' | 'md' | 'lg';
   src?: string;
   value?: any;
   className: string;
 }
 
 export const AvatarClickableTsx: React.FC<IAvatarProps> = React.forwardRef<any, IAvatarProps>(
-  ({ className, children, isSelected, onClick, size = 'md', src, value, ...rest }, ref) => {
+  ({ className, children, onClick, src, value }, ref) => {
     const avatarData = useMemo<string>(() => (children || '- -').toString(), [children]);
 
     const handleClick = useCallback(
@@ -33,13 +30,13 @@ export const AvatarClickableTsx: React.FC<IAvatarProps> = React.forwardRef<any, 
     );
 
     return (
-      <div {...rest}>
+      <>
         <ButtonBase component={onClick ? 'button' : 'div'} ref={ref} onClick={handleClick}>
           <Avatar className={className} alt={avatarData} src={src}>
             {avatarData.substr(0, 2).toUpperCase()}
           </Avatar>
         </ButtonBase>
-      </div>
+      </>
     );
   }
 );
