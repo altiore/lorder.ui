06c265648d3ab6bfc30ec0fcb304d2e8ef6e9035
jolyfan <c0d3ar@ya.ru>
2021-01-09 13:24:04 +0500

    feat: added beDev and feDev priority in roles list

--- src/#/@store/@common/helpers/get-member-role.ts
+++ src/#/@store/@common/helpers/get-member-role.ts
@@ -8,6 +8,8 @@ export function getMemberRole(roles?: IProjectRole[]): string {
   const arch = roles.find(el => el.roleId === ROLE_FLOW.ARCHITECT);
   const mainDev = roles.find(el => el.roleId === ROLE_FLOW.DEVELOPER);
   const designer = roles.find(el => el.roleId === ROLE_FLOW.DESIGNER);
+  const beDev = roles.find(el => el.roleId === ROLE_FLOW.BE_DEVELOPER);
+  const feDev = roles.find(el => el.roleId === ROLE_FLOW.FE_DEVELOPER);
 
-  return MAP_ROLE[(arch || mainDev || designer)?.roleId || roles[0].roleId];
+  return MAP_ROLE[(arch || mainDev || designer || beDev || feDev)?.roleId || roles[0].roleId];
 }
