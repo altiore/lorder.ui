532830f0dd166658e94f838c80bac66212d921a2
Razzwan <razvanlomov@gmail.com>
2021-09-18 12:34:57 +0300

    fix(get-member-role): fix get-memeber-role

--- src/#/@store/@common/helpers/get-member-role.ts
+++ src/#/@store/@common/helpers/get-member-role.ts
@@ -6,14 +6,10 @@ export function getMemberRole(roles?: IProjectRole[]): string {
   }
 
   const arch = roles.find(el => el.roleId === ROLE_FLOW.ARCHITECT);
-  const mainDev = roles.find(el => el.roleId === ROLE_FLOW.DEVELOPER);
+  const mainDev = roles.find(el => el.roleId === ROLE_FLOW.LEAD_DEVELOPER);
   const designer = roles.find(el => el.roleId === ROLE_FLOW.DESIGNER);
   const beDev = roles.find(el => el.roleId === ROLE_FLOW.BE_DEVELOPER);
   const feDev = roles.find(el => el.roleId === ROLE_FLOW.FE_DEVELOPER);
 
-  console.log('roles', {
-    ROLE_FLOW,
-    roles,
-  });
   return MAP_ROLE[(arch || mainDev || designer || beDev || feDev)?.roleId || roles[0].roleId];
 }
