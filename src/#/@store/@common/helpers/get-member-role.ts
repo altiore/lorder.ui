d0a82544dbe9d297e98f324dbf038e0ece6c0514
Razzwan <razvanlomov@gmail.com>
2021-09-18 12:33:26 +0300

    fix(simple-roles): fix simple roles names

--- src/#/@store/@common/helpers/get-member-role.ts
+++ src/#/@store/@common/helpers/get-member-role.ts
@@ -11,5 +11,9 @@ export function getMemberRole(roles?: IProjectRole[]): string {
   const beDev = roles.find(el => el.roleId === ROLE_FLOW.BE_DEVELOPER);
   const feDev = roles.find(el => el.roleId === ROLE_FLOW.FE_DEVELOPER);
 
+  console.log('roles', {
+    ROLE_FLOW,
+    roles,
+  });
   return MAP_ROLE[(arch || mainDev || designer || beDev || feDev)?.roleId || roles[0].roleId];
 }
--- src/@types/i.user-role.ts
+++ src/@types/i.user-role.ts
@@ -1,6 +1,7 @@
 export enum ROLE_FLOW {
   ARCHITECT = 'architect',
-  DEVELOPER = 'dev-full-lead',
+  LEAD_DEVELOPER = 'dev-full-lead',
+  FS_DEVELOPER = 'dev-full',
   TESTER = 'qa-engineer',
   DESIGNER = 'designer',
   FE_DEVELOPER = 'dev-front-end',
@@ -10,9 +11,10 @@ export enum ROLE_FLOW {
 
 export const MAP_ROLE: { [key in ROLE_FLOW]: string } = {
   [ROLE_FLOW.ARCHITECT]: 'Архитектор',
-  [ROLE_FLOW.DEVELOPER]: 'Главный программист',
+  [ROLE_FLOW.LEAD_DEVELOPER]: 'Тим лид',
+  [ROLE_FLOW.FS_DEVELOPER]: 'Ведущий программист',
   [ROLE_FLOW.DESIGNER]: 'Дизайнер',
-  [ROLE_FLOW.TESTER]: 'Тестеровщик',
+  [ROLE_FLOW.TESTER]: 'Тестировщик',
   [ROLE_FLOW.FE_DEVELOPER]: 'Front End - программист',
   [ROLE_FLOW.BE_DEVELOPER]: 'Back End - программист',
   [ROLE_FLOW.REVIEWER]: 'Наблюдатель',
